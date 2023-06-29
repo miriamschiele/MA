574ef41bfcfc469b39933e67286907edd81f3a52
Miriam Schiele <m.schiele12@gmail.com>
2023-06-29 11:33:24 +0200

    debug mode

--- 2 Experiments/3 magpie/src/magpie.config.js
+++ 2 Experiments/3 magpie/src/magpie.config.js
@@ -1,11 +1,11 @@
 export default {
-  experimentId: '108',
+  experimentId: '',
   serverUrl: 'https://mcmpact.ikw.uni-osnabrueck.de/magpie/',
   socketUrl: 'wss://mcmpact.ikw.uni-osnabrueck.de/magpie/socket',
   // this will be used in prolific mode
   completionUrl: 'https://app.prolific.co/submissions/complete?cc=CZD6B65H',
   contactEmail: 'm.schiele12@gmail.com',
   // Either 'debug', 'directLink' or 'prolific'
-  mode: 'prolific',
+  mode: 'debug',
   language: 'en'
 };
