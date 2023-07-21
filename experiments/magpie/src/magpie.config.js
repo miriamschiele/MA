1a4308b91164a8aef708592bc2520bc99d440749
miriamschiele <m.schiele@gmail.com>
2023-07-21 18:48:16 +0100

    prolific URL added

--- experiments/magpie/src/magpie.config.js
+++ experiments/magpie/src/magpie.config.js
@@ -3,7 +3,7 @@ export default {
   serverUrl: 'https://magpie-cogsciprag.fly.dev//',
   socketUrl: 'wss://mcmpact.ikw.uni-osnabrueck.de/magpie/socket',
   // this will be used in prolific mode
-  completionUrl: 'https://app.prolific.co/submissions/complete?cc=CZD6B65H',
+  completionUrl: 'https://app.prolific.co/submissions/complete?cc=C19MSPD9',
   contactEmail: 'm.schiele12@gmail.com',
   // Either 'debug', 'directLink' or 'prolific'
   mode: 'prolific',
